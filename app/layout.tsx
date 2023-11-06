9e5e772042bde47a57470f986a4f9794217a5f32
Md Tarvez Ansari <mdtarvezansari@Mds-MacBook-Air.local>
2023-11-06 08:12:32 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Header from '@/components/Header';
-import NavBar from '@/components/navbar';
+
 import 'remixicon/fonts/remixicon.css';
+import React from 'react';
+import Layout from '@/components/Layout';
+import NavBar from '@/components/navbar';
+import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,9 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <NavBar />
-        {children}
+        <div style={{ display: 'flex' }}>
+          <NavBar/>
+          <div style={{ flexGrow: 0 }}>
+            <Header />
+            <main style={{ padding: '20px' }}>
+              {children}
+            </main>
+          </div>
+          </div>
       </body>
     </html>
   )
