7ae3062018b5e8ab5c76ac8222a87002fc6aeb8f
Md Tarvez Ansari <mdtarvezansari@Mds-MacBook-Air.local>
2023-11-23 08:58:07 +0530

    update

---
+++ components/ActionMenu/index.tsx
@@ -0,0 +1,39 @@
+import { Menu, MenuItem, Typography } from '@mui/material';
+import React from 'react'
+
+interface ActionMenuProps {
+	id: string;
+	options: any
+}
+const ActionsMenu = (props: ActionMenuProps) => {
+	const { id, options } = props;
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = (event: any) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div>
+			<Typography onClick={handleClick} sx={{ cursor: "pointer" }}>
+				<i className="ri-more-2-fill" />
+			</Typography>
+			<Menu
+				anchorEl={anchorEl}
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+			>
+				{options.map((item: any) => (
+					<MenuItem onClick={()=>item.handler(id)}>{item.label}</MenuItem>
+				))}
+			</Menu>
+		</div>
+	);
+};
+
+export default ActionsMenu
---
+++ lib/tempData.ts
@@ -0,0 +1,39 @@
+const userData = Array.from({ length: 30 }, (_, index) => ({
+    id: index + 1,
+    username: `user${index + 1}`,
+    fullName: getRandomFullName(),
+    email: `user${index + 1}@example.com`,
+    mobile: getRandomMobile(),
+    status: index % 3 === 0 ? 'Active' : 'Inactive',
+    dateCreated: getRandomDate(),
+    lastLogin: getRandomDate(),
+}));
+
+function getRandomFullName() {
+    const firstNames = ['John', 'Alice', 'Bob', 'Emma', 'David', 'Sophia', 'Michael', 'Olivia', 'William', 'Ella'];
+    const lastNames = ['Doe', 'Johnson', 'Smith', 'White', 'Brown', 'Lee', 'Davis', 'Martin', 'Rodriguez', 'Taylor'];
+
+    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+    return `${randomFirstName} ${randomLastName}`;
+}
+
+function getRandomDate() {
+    const currentDate = new Date();
+    const randomDaysAgo = Math.floor(Math.random() * 365); // Random number of days ago
+    const pastDate = new Date(currentDate);
+    pastDate.setDate(currentDate.getDate() - randomDaysAgo);
+
+    return pastDate.toLocaleDateString();
+}
+
+function getRandomMobile() {
+    const prefix = '+91'; // Assuming a country code for simplicity
+    const randomNumbers = Math.floor(1000000000 + Math.random() * 9000000000); // Generates a 10-digit number
+  
+    return `${prefix}${randomNumbers}`;
+  }
+  
+
+export const tempUserData = userData;
