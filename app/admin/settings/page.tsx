9b3eaf3ddcf160beecedba05c3089aab5737004e
Md Tarvez Ansari <mdtarvezansari@192.168.1.3>
2024-02-06 13:16:43 +0530

    update

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,49 +1,37 @@
 "use client"
 
-import { Box, Button, Stack, styled } from '@mui/material'
+import { Box, Tabs, Tab } from '@mui/material'
 import React from 'react'
-import styles from './style.module.scss';
 import SettingsDetails from '@/components/SettingsDetails';
 import Faqs from '@/components/Faqs';
 
 
-const StyledButton = styled(Button)`
-  &:hover {
-    background: #1976d2;
-    color: #fff;
-  }
-`;
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 const Settings = () => {
-  const tabs = ['Settings', 'Faqs'];
-  const [tab, setTab] = React.useState(tabs[0]);
+  const [value, setValue] = React.useState(0);
+
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <Box sx={{ height: "100vh", overflowY: "scroll" }}>
-      <Box sx={{ marginBottom: '2.0625rem' }}>
-        <Stack direction="row" spacing="0.67rem" sx={{ marginBottom: 0 }}>
-          {tabs.map((tabtitle) => (
-            <StyledButton
-              className={styles.tabB}
-              style={{
-                textTransform: 'none',
-                borderBottomRightRadius: 0,
-                borderBottomLeftRadius: 0,
-                borderBottom: 0,
-              }}
-              key={tabtitle}
-              size="small"
-              onClick={() => setTab(tabtitle)}
-              variant={tabtitle === tab ? 'contained' : 'outlined'}
-              color={tabtitle === tab ? 'primary' : 'primary'} //
-            >
-              {tabtitle}
-            </StyledButton>
-          ))}
-        </Stack>
-        <hr className={styles.line} />
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 2 }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Settings" {...a11yProps(0)} />
+          <Tab label="Faqs" {...a11yProps(1)} />
+        </Tabs>
       </Box>
-      {tab === tabs[0] && <SettingsDetails />}
-      {tab === tabs[1] && <Faqs />}
+      {/* {value === 0 && <SettingsDetails />} */}
+      {value === 1 && <Faqs />}
 
     </Box>
   )
