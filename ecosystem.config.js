b18bdfaecfff799df34a9505346e4317a16b1110
Md Tarvez Ansari <mdtarvezansari@Mds-MacBook-Air.local>
2024-04-18 00:17:49 +0530

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  }],
+
+  deploy : {
+    production : {
+      key : 'myuride_server_chris.pem',
+      user : 'ubuntu',
+      host : '18.222.174.91',
+      ref  : 'origin/main',
+      repo : 'https://github.com/tabrez1991/myUride.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
